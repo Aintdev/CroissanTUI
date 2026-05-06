21654fb8b78e094bbb932601f8e1d0e94bcf9b48
Aintmee <85761348+Aintdev@users.noreply.github.com>
2026-05-06 16:44:42 +0200

    Add project files.

---
+++ CroissanTUI/include/ctui.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "../src/ctui_mod_enum.h"
+#include "../src/ctui_mod.h"
+#include "../src/ctui_config.h"
+#include "../src/ctui_print.h"
---
+++ CroissanTUI/src/ctui_config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace ctui {
+    extern bool rawMode;
+    static void enableRawMode();
+    static void disableRawMode();
+}
