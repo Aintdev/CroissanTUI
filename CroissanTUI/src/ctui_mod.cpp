0a4713b8cc41a11e90063e2b761722da3b0a4c3e
Aintmee <85761348+Aintdev@users.noreply.github.com>
2026-05-06 17:04:06 +0200

    Fixed static in ctui_config.cpp and Functions were undefined. also fixed some other minor bugs and performance issue.

--- CroissanTUI/src/ctui_mod.cpp
+++ CroissanTUI/src/ctui_mod.cpp
@@ -6,18 +6,20 @@ namespace ctui {
 	Mod::Mod(color::FG c) : code("\033[" + std::to_string((int)c) + "m") {};
 	Mod::Mod(color::BG c) : code("\033[" + std::to_string((int)c) + "m") {};
 	Mod::Mod(int x, int y) : code("\033[" + std::to_string(y) + ";" + std::to_string(x) + "f") {}
-	Mod::Mod(std::string x) : code(x) {}
+	Mod::Mod(std::string x) : code(std::move(x)) {}
 
 	const std::string Mod::getCode() const { return code; }
 
 	Mod Mod::mvCur(int x, int y) { return Mod(x, y); }
 	Mod Mod::clearPath(int x1, int line, int x2) {
 		std::string result = std::string("\x1b""7") + std::string("\033[") + std::to_string(line) + std::string(";") + std::to_string(x1) + std::string("H");
-		result += std::string(x2 - x1, ' ') + std::string("\x1b""8");
+		if (x2 < x1) std::swap(x1, x2);
+		int len = x2 - x1;
+		result += std::string(len, ' ') + std::string("\x1b""8");
 		return Mod(result);
 	}
 
-	Mod::operator const std::string& () const {
+	Mod::operator const std::string () const {
 		return code;
 	}
 }
