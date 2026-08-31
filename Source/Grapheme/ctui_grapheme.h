e82b63d8dc0b7032830db0214cc9508e2c265047
Aintdev <85761348+Aintdev@users.noreply.github.com>
2026-08-31 12:05:13 +0200

    docs: added doxygen comments to Grapheme and GraphemeView

--- Source/Grapheme/ctui_grapheme.h
+++ Source/Grapheme/ctui_grapheme.h
@@ -5,7 +5,17 @@ namespace ctui
 {
 	struct Grapheme {
 		std::string_view text;
+
+		/**
+		 * calculates visible length in terminal
+		 * @return width of grapheme in terminal
+		 */
 		size_t terminal_width() const;
+
+		/**
+		 * gets allocated bytes of grapheme
+		 * @return size in memory of grapheme
+		 */
 		size_t byte_count() const;
 	};
 }
