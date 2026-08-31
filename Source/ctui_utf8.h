b8b8b7ed0410b79ecd82c04437cb42896fc577fb
Aintdev <85761348+Aintdev@users.noreply.github.com>
2026-08-31 12:16:43 +0200

    docs: added documentation to ctui::utf8_display_width and ctui::mv_cursor

--- Source/ctui_utf8.h
+++ Source/ctui_utf8.h
@@ -4,12 +4,19 @@
 
 namespace ctui
 {
-    size_t utf8_display_width(const std::string_view text);
+	/**
+	 * The utf8_display_width function calculates the display width of 
+	 * a UTF-8 encoded string by summing the terminal widths of its graphemes.
+	 * @param text The text to calculate the width from
+	 * @return Width of text in Terminal
+	 */
+	size_t utf8_display_width(const std::string_view text);
 
-    /// <summary>
-    /// Moves cursor to given coordinates.
-    /// </summary>
-    /// <param name="x">X-Position to move to</param> 
-    /// <param name="y">Y-Position to move to</param>
+    /**
+     * Calculates ANSI Escape Sequences to move cursor to given coordinates.
+     * @param x X-Position to move to
+     * @param y Y-Position to move to
+     * @return ANSI Escape Sequences as std::string
+     */
     std::string mv_cursor(const size_t x, const size_t y);
 }
