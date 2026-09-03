b6ad3793edac2dc4531a84e267a03c972acda131
Aintdev <85761348+Aintdev@users.noreply.github.com>
2026-09-03 13:57:03 +0200

    docs: generate Doxygen documentation during setup

--- Scripts/Sample-Setup-Linux.sh
+++ Scripts/Sample-Setup-Linux.sh
@@ -3,6 +3,9 @@ set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+pushd "$SCRIPT_DIR/../Docs"
+doxygen Doxyfile
+
 pushd "$SCRIPT_DIR/../Sample"
 cmake -S . -B build -G "Unix Makefiles"
 popd
