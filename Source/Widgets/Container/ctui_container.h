41636923f1808578f1439884c47ee11ad120dd45
Aintdev <85761348+Aintdev@users.noreply.github.com>
2026-09-07 14:20:29 +0200

    docs: added doxygen comments to container and added getters to container

--- Source/Widgets/Container/ctui_container.h
+++ Source/Widgets/Container/ctui_container.h
@@ -1,18 +1,54 @@
 #pragma once
-#include "Widgets/ctui_widget.h"
+
 #include <vector>
 
 #include "ctui_defaults.h"
+#include "Widgets/ctui_widget.h"
+
 
 namespace ctui {
+	/**
+	 * Abstract base class for all Container components.
+	 * 
+	 * Container provides basic functionality to add and remove Widgets.
+	 */
 	struct Container : Widget, protected defaults::WidgetDefaults {
+	protected:
+		/// Index of Container::_children where focus is set to.
 		int _focus_index = kFocusIndex;
+
+		/// Children that the Container is the parent of
 		std::vector<Widget*> _children = {};
 		
+		Container() = default;
+	public:
+		// Getters
+		[[nodiscard]] int get_focus_index() const;
+		
+		[[nodiscard]] std::vector<Widget*>& get_children() const;
+
+		/**
+		 * Makes widget a child of this.
+		 * @param child Widget that should be made child.
+		 */
 		void make_child(Widget* child);
+
+		/**
+		 * Removes child Widget from Container.
+		 * @param child Widget that should be removed of Container.
+		 */
 		void remove(Widget* child);
 
+		/**
+		 * Renders alls children
+		 */
 		void render() override;
+
+		Container(const Container&) = delete;
+		Container& operator=(const Container&) = delete;
+		Container(Container&&) = delete;
+		Container& operator=(Container&&) = delete;
+
 		~Container() override;
 	};
 }
