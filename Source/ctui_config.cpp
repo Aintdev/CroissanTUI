1163931c6510c2b6a700372cb7d112cefeefc6c9
Aintmee <85761348+Aintdev@users.noreply.github.com>
2026-09-14 21:33:56 +0200

    feat(config): add RAII guard for raw mode

--- Source/ctui_config.cpp
+++ Source/ctui_config.cpp
@@ -7,51 +7,12 @@
 #include <windows.h>
 #include <conio.h>
 #else
-#include <termios.h>
 #include <unistd.h>
 #include <sys/ioctl.h>
-#include <stdio.h>
 #endif
 
-namespace ctui {
-    bool raw_mode = false;
-
-
-    void enable_raw_mode() {
-#ifdef _WIN32
-        HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-        DWORD mode;
-        GetConsoleMode(hStdin, &mode);
-        SetConsoleMode(hStdin, mode & ~(ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT));
-        HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-        GetConsoleMode(hOut, &mode);
-        SetConsoleMode(hOut, mode | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
-#else
-        termios t;
-        tcgetattr(STDIN_FILENO, &t);
-        t.c_lflag &= ~(ICANON | ECHO | IXON);
-        tcsetattr(STDIN_FILENO, TCSANOW, &t);
-#endif
-        std::ios::sync_with_stdio(false);
-    	raw_mode = true;
-        std::cout << "\033[2J\033[H";
-    }
-
-    void disable_raw_mode() {
-#ifdef _WIN32
-        HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-        DWORD mode;
-        GetConsoleMode(hStdin, &mode);
-        SetConsoleMode(hStdin, mode | ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT);
-#else
-        termios t;
-        tcgetattr(STDIN_FILENO, &t);
-        t.c_lflag |= (ICANON | ECHO);
-        tcsetattr(STDIN_FILENO, TCSANOW, &t);
-#endif
-        raw_mode = false;
-    }
-
+namespace ctui
+{
     std::pair<int, int> get_win_size() {
 #ifdef _WIN32
         CONSOLE_SCREEN_BUFFER_INFO csbi;
