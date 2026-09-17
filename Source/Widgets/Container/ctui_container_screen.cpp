c7a7a7bf504ffb90720fe6c28901e21bb0bf9d5f
Aintmee <85761348+Aintdev@users.noreply.github.com>
2026-09-17 11:18:12 +0200

    feat: impl stagecallback and other stuff

--- Source/Widgets/Container/ctui_container_screen.cpp
+++ Source/Widgets/Container/ctui_container_screen.cpp
@@ -6,7 +6,7 @@
 
 namespace ctui
 {
-	void Screen::update_bounds()
+	void Screen::update_bounds(const std::pair<int, int>& winsize)
 	{
 		const auto [fst, snd] = get_win_size();
 		_relative_bounds = Rect(0, 0, fst, snd);
@@ -25,35 +25,5 @@ namespace ctui
 		}
 	}
 
-	void Screen::run(const std::function<void(RunStage)>& stage_callback)
-	{
-		RawModeGuard rwg;
-		
-		auto win_size = get_win_size();
-		bool resized = false;
 
-		running = true;
-		while (running)
-		{
-			auto new_winsize = get_win_size();
-			if (new_winsize != win_size)
-			{
-				win_size = new_winsize;
-				this->update_bounds(); // update screen width and height
-				std::cout << "\033[?2026h" << "\033[2J\033[H"; // DEC Private Mode Set & Clear Screen
-				resized = true;
-			}
-
-			this->measure(win_size.first); // measure all widgets
-
-			this->resolve_bounds(0, 0);  // add the positions together to let the widgets know their absolute positions
-
-			this->render(); // print to screen
-			if (resized)
-			{
-				resized = false;
-				std::cout << "\033[?2026l";
-			}
-		}
-	}
 }
