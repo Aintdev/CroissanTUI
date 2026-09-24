305f3cf9f6c101a20b57e8854a6c5142c17d7f86
Aintmee <85761348+Aintdev@users.noreply.github.com>
2026-09-24 18:37:25 +0200

    docs(config): added documentation to signal_handler() and get_win_size

--- Source/ctui_config.h
+++ Source/ctui_config.h
@@ -7,7 +7,18 @@ namespace ctui {
     int get_signal_status();
     void reset_signal();
 
+    /**
+     * Default signal handler of CroissanTUI. Will set ctui::running
+     * to false and update signal_status.
+     * 
+     * Does not kill a program directly.
+     * @param sig signal
+     */
     void signal_handler(int sig);
 
+    /**
+     * Returns terminal size.
+     * @return Windows size as pair in x, y.
+     */
     std::pair<int, int> get_win_size();
 }
